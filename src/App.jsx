c5686012f308150868dc70f7f8e0ef2a011f51f8
Chillwithjosh <olabiyijoshua15@gmail.com>
2023-10-06 03:34:39 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,28 @@
+import { Route, Routes } from 'react-router-dom'
+import './App.css'
+import { Home } from './Pages/Home'
+import Navbar from './components/Navbar'
+import Login from './Pages/Login'
+import Signup from './Pages/Signup'
+import Account from './Pages/Account'
+
+// control + spacbar at the end of Navbar to import directly
+// 6 8 9
+
+export default function App() {
+
+  return (
+    <>
+    {/* <AuthContextProvider> */}
+      <Navbar/>
+      <Routes>
+        <Route path='/' element ={<Home/>} />
+        <Route path='/login' element={<Login/>} />
+        <Route path='/signup' element={<Signup/>} />
+        <Route path='/account' element={<Account/>} />
+      </Routes>
+    {/* </AuthContextProvider> */}
+    </>
+  )
+}
+
